3c981c2891f63827e03f942767aab8e611449848
Alexis Ozaeta <ozaetadev@gmail.com>
2021-07-25 11:00:36 -0600

    DEV-3 | Use of AuthProvider to have firebase context in the whole app.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { Router } from '@reach/router'
 // Pages
 import { Landing } from './pages/Landing'
 import { NotFound } from './pages/NotFound'
+import { CoachLogin } from './pages/Coach/Login'
 // Components
 
+// Providers
+import { AuthProvider } from './contexts/AuthContext'
 // Styles
 import './App.scss';
 
 export const App = () => (
-    <Router>
-        <Landing path="/" />
-        <NotFound default />
-    </Router>
+    <AuthProvider>
+        <Router>
+            <Landing path="/" />
+            <CoachLogin path="/coach/login" />
+            <NotFound default />
+        </Router>
+    </AuthProvider>
 );
