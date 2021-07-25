9ce3039a0126107822ded6fddf81afa69c43b08b
Alexis Ozaeta <ozaetadev@gmail.com>
2021-07-25 10:55:49 -0600

    DEV-3 | BackButton component. Arrow left/right icons.

---
+++ src/components/BackButton/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from '@reach/router'
+
+// Icons
+import { ArrowLeft, ArrowRight } from '../../assets/icons'
+// Styles
+import './BackButton.scss'
+
+export const ON_LEFT = 1
+export const ON_RIGHT = 2
+
+export const BackButton = ({
+    customClass = "",
+    path = "/",
+    text = "Regresar",
+    direction = ON_LEFT }) => (
+        <div className={`backButton${direction === ON_LEFT ? "-left" : "-right"}`}>
+            {
+                direction === ON_LEFT
+                ? <Link to={path}> <ArrowLeft /> {text} </Link>
+                : <Link to={path}> {text} <ArrowRight /> </Link>
+            }
+        </div>
+    )
