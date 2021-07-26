f43c84fed7a3b6c6b535fe0852565e61b0727e4e
Alexis Ozaeta <ozaetadev@gmail.com>
2021-07-25 23:29:10 -0600

    DEV-6 | Add toggle button to show/hide options in mobile and tablet screens.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import { OPTION_TYPE_BUTTON, OPTION_TYPE_LINK } from '../../constants/Navbar'
 import { NavbarLink } from './NavbarLink'
 import { NavbarButton } from './NavbarButton'
 import { NavbarLinkLogout } from './NavbarLinkLogout'
 import { Logo } from '../../assets/img/logo'
+import { Bars } from '../../assets/icons'
 
 import './Navbar.scss'
 
 export const Navbar = ({ path = "/", options = [] }) => {
+    const [isOpen, openMenu] = useState(false)
+    const [isMobileOrTabletScreen, setMobileOrTabletScreen] = useState(false)
+
+    function handleToggleMenu(e) {
+        e.preventDefault()
+        openMenu(!isOpen)
+    }
+
+    useEffect(() => {
+        const onResize = () => {
+            const isMobileOrTablet = window.innerWidth <= 768
+            setMobileOrTabletScreen(isMobileOrTablet)
+        }
+        window.addEventListener('resize', onResize)
+
+        onResize()
+    }, [])
+
     return (
         <nav className="navbar">
-            <div className="navbar__logo">
-                <Link to={path}>
-                    <Logo />
-                </Link>
+            <div className="navbar__header">
+                <div className="navbar__header__logo">
+                    <Link to={path}>
+                        <Logo />
+                    </Link>
+                </div>
+                <div className="navbar__header__toggleButton">
+                    <div onClick={handleToggleMenu}>
+                        <Bars />
+                    </div>
+                </div>
             </div>
-            <ul className="navbar__options">
+            <ul className={`navbar__options${isMobileOrTabletScreen ? (isOpen ? "-show" : "-hide") : ""}`}>
                 {
                     options.map(function (option, index) {
                         return (
